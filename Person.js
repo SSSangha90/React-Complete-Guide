f53617c5cd15971b6b323c33e464f0af30f3e3fe
Sukh Sangha <37102418+SSSangha90@users.noreply.github.com>
2019-07-28 14:17:07 +0100

    Update Person.js

--- Person.js
+++ Person.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import './Person.css'
+
 // static component
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.newChange} value={props.name}/>
         </div>
     )
 }
